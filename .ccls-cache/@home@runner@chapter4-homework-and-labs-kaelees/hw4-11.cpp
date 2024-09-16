2e6626c7b84f448d39125a57ce7459c128250a75
kaelees <kaeleestorozuk@my.tridenttech.edu>
2024-09-16 21:38:38 +0000

    finished hw4-11
    done

--- .ccls-cache/@home@runner@chapter4-homework-and-labs-kaelees/hw4-11.cpp
+++ .ccls-cache/@home@runner@chapter4-homework-and-labs-kaelees/hw4-11.cpp
@@ -1,10 +1,24 @@
 // Introductory11.cpp - displays ending balance
-// Created/revised by <your name> on <current date>
+// Created/revised by kaelee storozuk on 9/16/24
 
 #include <iostream>
 using namespace std;
 
 int main() {
+  double salePrice = 0.0;
+  double taxRate = 0.0;
+  double SalesTax = 0.0;
+  double cost = 0.0;
+
+  cout << "Enter the sale price: ";
+  cin >> salePrice;
+  cout << "Eneter the sales tax rate: ";
+  cin >> taxRate;
+
+  SalesTax= salePrice * taxRate;
+  cost = salePrice + SalesTax;
+
+  cout << "Cost: $" << cost << endl;
   
 
   return 0;
