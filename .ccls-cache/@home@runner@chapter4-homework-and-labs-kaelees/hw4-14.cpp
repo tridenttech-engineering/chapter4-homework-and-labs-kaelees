a019618195c522fd20ee0b0b4f98b9b625a2bc09
kaelees <kaeleestorozuk@my.tridenttech.edu>
2024-09-16 22:23:55 +0000

    lab4-6 and hw4-14
    retry

--- .ccls-cache/@home@runner@chapter4-homework-and-labs-kaelees/hw4-14.cpp
+++ .ccls-cache/@home@runner@chapter4-homework-and-labs-kaelees/hw4-14.cpp
@@ -5,6 +5,39 @@
 using namespace std;
 
 int main() {
+  int smallPizzas = 0;
+  int mediumPizzas = 0;
+  int largePizzas = 0;
+  int familyPizzas = 0;
+  int totalPizzas = 0;
+  double smallPizzasPercent = 0.0;
+  double mediumPizzasPercent = 0.0;
+  double largePizzasPercent = 0.0;
+  double familyPizzasPercent = 0.0;
+
+  cout << "Number of small pizzas sold: ";
+  cin >> smallPizzas;
+  cout << "Number of medium pizzas sold: ";
+  cin >> mediumPizzas;
+  cout << "Number of large pizzas sold: ";
+  cin >> largePizzas;
+  cout << "Number of family pizzas sold: ";
+  cin >> familyPizzas;
+
+  totalPizzas = smallPizzas + mediumPizzas + largePizzas + familyPizzas;
+  
+
+  cout << "Total pizzas sold: " << totalPizzas << endl;
+
+  smallPizzasPercent = smallPizzas / totalPizzas * 100;
+  mediumPizzasPercent = mediumPizzas / totalPizzas * 100;
+  largePizzasPercent = largePizzas / totalPizzas * 100;
+  familyPizzasPercent = familyPizzas / totalPizzas * 100;
+  
+  cout << "Small pizzas contribution: " << smallPizzasPercent << endl;
+  cout << "Medium pizzas contribution: " << mediumPizzasPercent << endl;
+  cout << "Large pizzas contribution: " << largePizzasPercent << endl;
+  cout << "Family pizzas contribution: " << familyPizzasPercent << endl;
   
 
   return 0;
