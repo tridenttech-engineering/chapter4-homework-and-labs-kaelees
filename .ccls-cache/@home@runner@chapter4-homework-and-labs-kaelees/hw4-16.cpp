eb2969ef66bc2256b4fa0afcc250d80d375db450
kaelees <kaeleestorozuk@my.tridenttech.edu>
2024-09-16 22:43:57 +0000

    tried hw4-14 and hw4-16
    tried

--- .ccls-cache/@home@runner@chapter4-homework-and-labs-kaelees/hw4-16.cpp
+++ .ccls-cache/@home@runner@chapter4-homework-and-labs-kaelees/hw4-16.cpp
@@ -1,5 +1,5 @@
 //Advanced16.cpp
-//Created/revised by <your name> on <current date>
+//Created/revised by kaelee storozuk on 9/16/24
 
 #include <iostream>
 #include <iomanip>
@@ -7,7 +7,26 @@ using namespace std;
 
 int main()
 {
+  double store1Int = 0.0;
+  double store2Int = 0.0;
+  double totalInt = 0.0;
+  double store1 = 0.0;
+  double store2 = 0.0;
+  const double increase = 0.62;
 
+  cout << "Store 1 sales: ";
+  cin >> store1;
+  cout << "Store 2 sales: ";
+  cin >> store2;
+
+  store1Int = static_cast<int>(store1 / 100.0 * increase * 100 + .5);
+  store2Int = static_cast<int>(store2 / 100.0 * increase * 100 + .5);
+  totalInt = store1Int + store2Int;
+  totalInt = totalInt / 2;
+
+  cout << "Total amount: $" << totalInt << endl;
+  
+  
   return 0;
 }	//end of main function
 
